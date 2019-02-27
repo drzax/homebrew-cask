fe6a1335a1653e5743996632f1b67a1040f81ed7
Simon Elvery <simon@elvery.net>
2019-02-27 11:45:04 +1000

    Add autoEdit2 v1.0.18

---
+++ Casks/autoedit2.rb
@@ -0,0 +1,10 @@
+cask 'autoedit2' do
+  version '1.0.18'
+  sha256 'f4b7cb217eafbc2ddedcb07e5391ec79a91809fb7fe30ba6da6b9b08b6e43a72'
+
+  url 'https://github.com/OpenNewsLabs/autoEdit_2/releases/download/1.0.18/autoEdit2-1.0.18-mac.zip'
+  name 'autoEdit2'
+  homepage 'http://www.autoedit.io/'
+
+  app 'autoEdit2.app'
+end
